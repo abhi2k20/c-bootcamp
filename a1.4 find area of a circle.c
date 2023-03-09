764a8a0f6b775169c753867595b6ed0bf346c965
Abhishek Kumar <74039075+abhi2k20@users.noreply.github.com>
2023-03-09 16:43:43 +0530

    Add files via upload

---
+++ a1.4 find area of a circle.c
@@ -0,0 +1,11 @@
+//find area of a circle
+#include<stdio.h>
+int main()
+{
+	int r;
+	float area;
+	scanf("%d",&r);
+	area=3.14*r*r;
+	printf("Area of circle is %f having radius %d",area,r);
+	return 0;
+}
